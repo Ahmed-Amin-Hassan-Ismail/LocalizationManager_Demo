1bb5a4eeb502b62c775732b492df7e268966efe1
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-08-28 15:58:14 +0200

    add some components

---
+++ LocalizationManager_Demo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LocalizationManager_Demo
+//
+//  Created by Ahmed Amin on 28/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
+
+    //MARK: - IBActions
+    @IBAction func switchLanguageAction(_ sender: UIButton) {
+        // to switch between two language
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
